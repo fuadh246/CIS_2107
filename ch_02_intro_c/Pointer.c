6cd72b484772536219bdad7b7935c09f82cc792a
fuadh246 <93026516+fuadh246@users.noreply.github.com>
2023-03-16 03:37:53 -0400

    race

--- ch_02_intro_c/Pointer.c
+++ ch_02_intro_c/Pointer.c
@@ -4,9 +4,29 @@
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
+#include <ctype.h>
+#include <stddef.h>
+void cubeByreference(int *nPrt);
+void convertToUpper(char *sPrt);
 int main(void){
-    char *fuad = "fuad";
+    int number=5;
+    printf("%d\n",number);
+    cubeByreference(&number);
+    printf("%d\n",number);
 
+    char string[] = "cHaRaCters and $32.98";
+    printf("The string before conversion is: %s", string);
+    convertToUpper(string);
+    printf("\nThe string after conversion is: %s\n", string);
 
     return 0;
+}
+void cubeByreference(int *nPrt){
+    *nPrt = *nPrt * *nPrt * *nPrt;
+}
+void convertToUpper(char *sPrt){
+    while (*sPrt != '\0'){
+        *sPrt = toupper(*sPrt);
+        ++sPrt;
+    }
 }
